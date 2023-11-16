9875f4cbb618a331bd57b7ecfbd970dfd19df8e7
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-11-16 03:47:06 -1100

    Sidebar and Navbar Implemented

---
+++ app/(dashboard)/_components/Logo.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+const Logo = () => {
+  return (
+      <Image 
+        height={130}
+        width={130}
+        alt="logo"
+        src="/ipsum.svg"
+      />
+  )
+}
+
+export default Logo
