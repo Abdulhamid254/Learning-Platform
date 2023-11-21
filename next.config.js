5ce907e89a2109ea398bf2c5f878c6607e3a86c7
Abdulhamid254 <nassurabdulhamid49@gmail.com>
2023-11-20 22:38:23 -1100

    Uploadthing & imageurl implemented

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        dormains: [
+            "utfs.io"
+        ]
+    }
+}
 
 module.exports = nextConfig
